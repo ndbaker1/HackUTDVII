63652dca6b321e56998d619b784e88fec9f621c9
Nick Baker <ndbaker1@outlook.com>
2021-02-27 17:07:38 -0600

    added base code for database connection after setting up cockroach db

---
+++ src/certs/ui-code.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+var pg = require('pg')
+
+// Connect to the bank database.
+var config = {
+  user: 'app_user',
+  host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+  database: 'mild-pig-985.hackutdvii',
+  port: 26257,
+  ssl: {
+    ca: fs.readFileSync('./ca.crt')
+      .toString(),
+    key: fs.readFileSync('./client.app_user.key')
+      .toString(),
+    cert: fs.readFileSync('./client.app_user.crt')
+      .toString()
+  }
+}
+
+// Create a pool.
+const pool = new pg.Pool(config)
+
+pool.query('SELECT * FROM hackutdvii WHERE item = \'shovel\'', [1], (err, res) => {
+  if (err) {
+    throw err
+  }
+  console.log('user:', res.rows[0])
+})
+
