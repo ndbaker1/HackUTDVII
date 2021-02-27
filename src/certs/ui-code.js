c31df12cb09cad6ee987800e33136e78618b480f
Nick Baker <ndbaker1@outlook.com>
2021-02-27 17:45:59 -0600

    update git-ignore and switch to password auth

--- src/certs/ui-code.js
+++ src/certs/ui-code.js
@@ -7,25 +7,20 @@ var config = {
   host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
   port: 26257,
   database: 'mild-pig-985.hackutdvii',
+  password: fs.readFileSync('./app_user.pass').toString(),
   ssl: {
-    ca: fs.readFileSync('./ca.crt')
-      .toString(),
-    key: fs.readFileSync('./client.app_user.key')
-      .toString(),
-    cert: fs.readFileSync('./client.app_user.crt')
-      .toString()
+    rejectUnauthorized: false,
   },
-
-
 }
 
 // Create a pool.
 const pool = new pg.Pool(config)
 
-pool.query('SELECT * FROM hackutdvii WHERE item = \'shovel\'', [1], (err, res) => {
+pool.query('SELECT * FROM lendings where item = \'item1\'', (err, res) => {
   if (err) {
     throw err
   }
-  console.log('user:', res.rows[0])
+  console.log('user:', res.rows)
+  pool.end()
 })
 
